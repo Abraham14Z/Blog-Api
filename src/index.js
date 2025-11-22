5d014501870483a838cca575f098ad4da81246bd
unknown <abrish02@gmail.com>
2025-11-22 14:18:56 +0800

    adding importing path and Loads variables from .env file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 const dotenv = require('dotenv');
+const path = require('path');//creating safe file paths across all operating systems.
+const envFound = dotenv.config({ path: path.resolve(process.cwd(), '.env') });
